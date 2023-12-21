787423edfa7bce2c9127383e8f73401335495593
Brendan Saager <bsaager@c4b5c6.42berlin.de>
2023-12-21 20:34:17 +0100

    updates

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bsaager <bsaager@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 09:31:06 by bsaager           #+#    #+#             */
-/*   Updated: 2023/12/21 16:41:24 by bsaager          ###   ########.fr       */
+/*   Updated: 2023/12/21 20:34:06 by bsaager          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,24 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	i;
 	size_t	j;
-	char	*fail;
-	char	len;
+	char	*str;
 
 	i = 0;
 	j = 0;
 	if (!s1 || !s2)
 		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	if (!(fail = (char*)malloc(sizeof(char) * (len + 1))))
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
 		return (NULL);
 	while (s1[i] != '\0')
 	{
-		fail[i] = s1[i];
+		str[i] = s1[i];
 		i++;
 	}
 	while (s2[j] != '\0')
-		fail[i++] = s2[j++];
-	fail[i] = '\0';
-	return (fail);
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	return (str);
 }
