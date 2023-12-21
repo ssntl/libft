85905012862e6c9c8cfc6464c83e5ece4de78ac6
Brendan Saager <bsaager@c4c6c4.42berlin.de>
2023-12-22 00:15:37 +0100

    finished strtrim and started split

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,58 @@
 /*   By: bsaager <bsaager@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 09:11:09 by bsaager           #+#    #+#             */
-/*   Updated: 2023/12/21 19:28:24 by bsaager          ###   ########.fr       */
+/*   Updated: 2023/12/22 00:12:40 by bsaager          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+// count words or substrings
+size_t	count_words(char const *s, char c)
+{
+	size_t	words;
+	int		flag;
+
+	words = 0;
+	while (*s)
+	{
+		flag = 0;
+		while (*s == c && *s)
+			s++;
+		while (*s != c && *s)
+		{
+			if (flag == 0)
+			{
+				words++;
+				flag = 1;
+			}
+			s++;
+		}
+	}
+	return (words);
+}
 
 char	**ft_split(char const *s, char c)
 {
+	int	len;
+	int	i;
 
+	len = ft_strlen(s);
+	i = 0;
+	while (i < len)
+	{
+		
+	}
+
+//malloc the words by passing through string again and counting len of substr
+
+//fill the new substrings with the words
+//array ends with NULL pointer
+}
+
+
+int	main()
+{
+	char *s = "Hello, World! "
+	**ft_split(*s, ' ');
+	return (0);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: bsaager <bsaager@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 19:22:51 by bsaager           #+#    #+#             */
-/*   Updated: 2023/12/21 19:37:43 by bsaager          ###   ########.fr       */
+/*   Updated: 2023/12/21 23:25:30 by bsaager          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	int		start;
+	int		end;
+	char	*noset;
+
+	start = 0;
+	end = ft_strlen(s1) -1;
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	if (!*s1)
+		return (ft_strdup(s1));
+	while (*(s1 + start) && ft_strchr(set, *(s1 + start)))
+		++start;
+	while (end >= 0 && ft_strchr(set, *(s1 + end)))
+		--end;
+	if (start > end)
+		return (ft_strdup(""));
+	noset = malloc((end - start) + 2);
+	if (NULL == noset)
+		return (NULL);
+	ft_strlcpy(noset, s1 + start, (end - start) + 2);
+	return (noset);
 }
